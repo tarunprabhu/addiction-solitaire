0e2f97581b3f003691a2acbf95f89c1e47dbb0d5
Tarun Prabhu <tarun.prabhu@gmail.com>
2019-06-09 10:28:03 -0500

    Added license

--- addiction/qt/ui.py
+++ addiction/qt/ui.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
+# Addiction Solitaire
+#
+# Copyright (C) 2019, Tarun Prabhu <tarun.prabhu@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 
 from ..game_ui import GameUI
-from ..settings_ui import SettingsUI
+from ..types import Suit, Face, Direction, Point
 
 class GameQt(GameUI):
     # Game
